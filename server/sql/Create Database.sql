ed266cc651c1ecbd75fda0a6ba75b9f46e0c3864
Skyler Young <skylerdyoung95@gmail.com>
2020-11-16 13:23:33 -0500

    database

--- server/sql/Create Database.sql
+++ server/sql/Create Database.sql
@@ -13,7 +13,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 -- -----------------------------------------------------
--- Schema sql9373442
+-- Schema sql9375420
 -- -----------------------------------------------------
 USE `mydb` ;
 
